20946507904111fb52b55960fe02753c27890be1
TiciaChance <laticia.chance@gmail.com>
2016-12-04 11:04:12 -0500

    added Core Data to project

--- WhoopToDo/ViewController.swift
+++ WhoopToDo/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
-
+    
+    var list: [NSManagedObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        coreDataInit()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func coreDataInit() {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let list = List(context: context)
+        
+        //list.item = "master protocols and delegates"
+        appDelegate.saveContext()
+        
+        do {
+            
+            let result = try context.fetch(List.fetchRequest())
+            let lists = result as! [List]
+            
+            for list in lists {
+                
+                print("Item: \(list.item)")
+            }
+        } catch {
+            print("Error")
+        }
+        
     }
-
-
+    
+    
 }
 
